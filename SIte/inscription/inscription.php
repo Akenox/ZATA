4b4bee640f6c22ed2ffe85627bed56509aa7981c
timotheemillot <timothee.millot@gmail.com>
2022-10-27 21:38:20 +0200

    bdd + formulaire fonctionnel

--- SIte/inscription/inscription.php
+++ SIte/inscription/inscription.php
@@ -1,25 +1,63 @@
 <?php
 
 
+/*$bdd = new PDO(
+    'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+    'grp-223',
+    'nkksqopb',
+);*/
+
+$bdd = new PDO(
+    'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+    'root', 
+    '', 
+);
 
 include_once("Inscription.html");
-setcookie("idc", "", time() - 3600); //destruction des cookies
-setcookie("pwc", "", time() - 3600);
-$a = "";
-$b = "";
 
-if (isset($_POST['id'])){
-    setcookie('idc',$_POST['id'], time() + 60 * 60 * 24, '/');
-}
 
-else if (isset($_POST['password'])){
-    setcookie('pwc,',$_POST['password'], time() + 60 * 60 * 24,'/');
-}
 
 
 if(isset($_POST['submit']))
 {
-    header('location:../connexion/code.html');
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $numero = $_POST['numero'];
+    $permis = $_POST['permis'];
+    $identifiant = $_POST['id'];
+    $motdepasse = $_POST['password'];
+
+    $reqIdExists = 'SELECT Identifiant FROM Compte WHERE Identifiant = :idTyped';
+    $reqPending = $bdd->prepare($reqIdExists);
+    $reqPending->execute([
+        'idTyped' => $identifiant,
+
+    ]);
+    $result = $reqPending->fetch();
+    if (isset($result['Identifiant']))
+    {
+        echo 'Identifiant déjà utilisé';
+    }
+    else 
+    {
+        $requete = 'INSERT INTO Compte(Prenom, Nom, Email, Numero, Permis, Identifiant, MotDePasse)  VALUES (:Prenom, :Nom, :Email, :Numero, :Permis, :Identifiant, :MotDePasse)';
+
+        $insertRecipe = $bdd->prepare($requete);
+
+        $insertRecipe->execute([
+            'Prenom' => $prenom,
+            'Nom' => $nom,
+            'Email' => $email,
+            'Numero' => $numero,
+            'Permis' => $permis,
+            'Identifiant' => $identifiant,
+            'MotDePasse' => $motdepasse,
+        ]);
+        header('location:../connexion/code.html');
+    }
+
+    
 }
 
 ?>
