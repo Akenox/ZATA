ba79bc4ca0396a314aef95926c917f25fcb6d697
timotheemillot <timothee.millot@gmail.com>
2022-11-23 15:37:12 +0100

    Liste des véhicules + Ajout de véhicules à la list

---
+++ SIte/PageVehicule/AddCar/AddCar.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+
+if ($_SESSION['bdd'] == "local")
+{
+    $bdd = new PDO(
+        'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+        'root', 
+        '' 
+    );
+}
+else
+{
+    $bdd = new PDO(
+        'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+        'grp-223', 
+        'nkksqopb' 
+    );
+}
+
+$marque = $_POST['marque'];
+$modele = $_POST['modele'];
+$immatriculation = $_POST['immatriculation'];
+$site = $_POST['site'];
+$carburant = $_POST['carburant'];
+$miseenservice = $_POST['miseenservice'];
+$critair = $_POST['critair'];
+$assurance = $_POST['assurance'];
+$puissance = $_POST['puissance'];
+$ageparc = $_POST['ageparc'];
+
+$requete = 'INSERT INTO Vehicule(Marque, Modele, Immatriculation, Site, Carburant, MiseEnService, Critair, Assurance, Puissance, AgeParc) VALUES (:Marque, :Modele, :Immatriculation, :Site, :Carburant, :MiseEnService, :Critair, :Assurance, :Puissance, :AgeParc)';
+
+$requete = $bdd->prepare($requete);
+
+$requete->execute([
+
+    'Marque' => $marque,
+    'Modele' => $modele,
+    'Immatriculation' => $immatriculation,
+    'Site' => $site,
+    'Carburant' => $carburant,
+    'MiseEnService' => $miseenservice,
+    'Critair' => $critair,
+    'Assurance' => $assurance,
+    'Puissance' => $puissance,
+    'AgeParc' => $ageparc,
+
+]);
+
+if (isset($_POST['submit']))
+{
+    require_once('AddCar.html');
+    
+}
+
+?>
---
+++ SIte/PageVehicule/vehicule.php
@@ -0,0 +1,143 @@
+<?php
+
+session_start();
+
+if ($_SESSION['bdd'] == "local")
+{
+    $bdd = new PDO(
+        'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+        'root', 
+        '' 
+    );
+}
+else
+{
+    $bdd = new PDO(
+        'mysql:host=localhost;dbname=grp-223_s3_sae;charset=utf8',
+        'grp-223', 
+        'nkksqopb' 
+    );
+}
+
+
+function CarTable($bdd) : string
+{
+    $bool = true;
+    $res = "";
+    $i = 1;
+    while($bool)
+    {
+        $req = 'SELECT*FROM Vehicule WHERE ID = :i';
+        $reqPending = $bdd->prepare($req);
+        $reqPending->execute(['i' => $i]);
+        $reqres = $reqPending->fetch();
+        if (isset($reqres[1]))
+        {
+            $res .= "<tr>
+                    <td>" . $reqres[1] . "</td>
+                    <td>" . $reqres[2] . "</td>
+                    <td>" . $reqres[3] . "</td>
+                    <td>" . $reqres[4] . "</td>
+                    <td>" . $reqres[5] . "</td>
+                    <td>" . $reqres[6] . "</td>
+                    <td>" . $reqres[7] . "</td>
+                    <td>" . $reqres[8] . "</td>
+                    <td>" . $reqres[9] . "</td>
+                    <td>" . $reqres[10] . "</td>
+                  <tr>
+                    ";
+            $i++;
+        }
+        else{
+            $bool = false;
+        }
+    }
+    return $res;
+}
+$_SESSION['function'] = CarTable($bdd); // permet d'afficher la fonction CarTable plus bas
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vehicule.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <img src="../res/logo.png" class="logo" alt="">
+            <ul>
+                <li><a href="../accueil/PageAccueil.php">Accueil</a></li>
+                <li><a href="Deconnexon.php">Se Deconnecter</a></li>
+            </ul>
+            <a href="../Compte/PageCompte.php"><img  src="../res/compte.png" class="compte" alt="" ></a>
+        </nav>
+    <h1>Liste de véhicules</h1>
+    
+
+
+
+
+    <table id="listcar">
+        <tr>
+            <th>Marque</th>
+            <th>Modele</th>
+            <th>Immatriculation</th>
+            <th>Site</th>
+            <th>Carburant</th>
+            <th>Date mise en service</th>
+            <th>Vignette Critair</th>
+            <th>Assurance</th>
+            <th>Puissance</th>
+            <th>Age Parc</th>
+        <tr>
+    
+
+        <?php
+        echo $_SESSION['function'];
+        ?>
+        
+    </table>    
+    <a href="AddCar/AddCar.html">Ajouter un véhicule</a>
+
+
+
+
+
+
+
+
+
+    <!-- FOOTER -->
+
+    <footer>
+        <div id="divtopfooter">
+        <div id="divhelp">
+            <span id="needhelptext">Besoin d'aide ? </span>
+            <a id="needhelplink" href="#">Contactez-nous !</a> 
+        </div>
+        <a id="policonf" href="../PoliConf/policonf.html">Politique de confidentialité<a>
+        <div id="langue">
+            <label for="langues">Langue:</label>
+            <select name="langues" id="">
+            <option value="">Français</option>
+            <option value="">Anglais</option>
+            </select>
+        </div>
+        </div>
+        
+        <p id="copyright">Copyright 2022 -- AirPur -- Tous droits réservés</p>
+    
+    </footer>
+
+    <!-- FOOTEREND -->
+</body>
+
+</html>
