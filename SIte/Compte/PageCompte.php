0e811ef9f3a4c2c620dc2c89ff5a2a0c026a4a52
Zeynelyz39 <zeynel_yazan@etu.u-bourgogne.fr>
2022-11-25 17:43:07 +0100

    Amélioration de la page d'information personnels

--- SIte/Compte/PageCompte.php
+++ SIte/Compte/PageCompte.php
@@ -1,5 +1,3 @@
-
-
 <?php
 require_once('../fonctions.php');
 include('PageCompte.html');
@@ -14,9 +12,7 @@ $bdd = Fonctions::InitBDD();
 
 $reqInfo = 'SELECT * FROM Compte WHERE Identifiant = :idSearch';
 $reqPending = $bdd->prepare($reqInfo);
-$reqPending->execute([
-    'idSearch' => $identifiant,
-    ]);
+$reqPending->execute(['idSearch' => $identifiant,]);
 $infos = $reqPending->fetch();
 
 $prenom =  $infos['Prenom'];
@@ -31,23 +27,36 @@ $prenom[0] = strtoupper($prenom[0]);
 $nom[0] = strtoupper($nom[0]);
 
 
-
-
-
-
 echo "
+
+<link rel=\"stylesheet\" href=\"PageCompte.css\">
+
 
 <div id=\"divform\">
 <ul>
-    <li class=\"oui\"><a >   Nom : </a> <a class=\"infos\"> $prenom </a> </li>              
+    <li class=\"oui\"><a >   Nom : </a> <a class=\"infos\"> $prenom </a> </li>
     <li class=\"oui\"><a >Prénom :</a><a class=\"infos\"> $nom</a></li>
+</ul>
+</div>
+
+    <div id=\"divform1_\">
+<ul>
+
     <li class=\"oui\"><a > Email : </a><a class=\"infos\"> $email</a></li>
     <li class=\"oui\"><a >Numéro : </a><a class=\"infos\"> $num</a></li>
+
+</ul>
+</div>
+
+<div id=\"divform2\">
+<ul>
     <li class=\"oui\"><a >Date de naissance : </a><a class=\"infos\"> $naissance </li>
-    <li class=\"oui\"><a >Identifiant     : </a><a class=\"infos\"> $id </li>
+    <li class=\"oui\"><a > Identifiant : </a><a class=\"infos\"> $id </li>
     <li class=\"oui\"><a >Mot de passe : </a><a class=\"infos\">$mdp</li>
 </ul>
-</div>";
+</div>
+";
+
 
 echo "
 <footer>
@@ -58,16 +67,16 @@ echo "
         </div>
         <a id=\"policonf\" href=\"../PoliConf/policonf.html\">Politique de confidentialité<a>
         <div id=\"langue\">
-            <label for=\"langues\">Langue:</label>
-            <select name=\"langues\" id=\"\">
-            <option value=\"\">Français</option>
-            <option value=\"\">Anglais</option>
+            <label for=\"langues\"> Langue : </label>
+            <select name=\"langues\" id="">
+            <option value="">Français</option>
+            <option value="">Anglais</option>
             </select>
         </div>
-    </div>      
+    </div>
     <p id=\"copyright\">Copyright 2022 -- AirPur -- Tous droits réservés</p>
 </footer> 
-"
+";
 
 
 
