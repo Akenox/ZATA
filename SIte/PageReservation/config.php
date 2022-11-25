ca4a24190743c4ed6c69a7222ef6f8f507e92700
adammota <adam.mota@iut-dijon.u-bourgogne.fr>
2022-11-25 11:54:07 +0100

    Création table reservation

--- SIte/PageReservation/config.php
+++ SIte/PageReservation/config.php
@@ -3,11 +3,49 @@ require_once('../fonctions.php');
 
 session_start();
 
+
 $Identifiant = $_SESSION['login'];
 
+Fonctions::CheckIfNotLoggedIn($_SESSION['login']);
+$bdd = Fonctions::InitBDD();
 
-    Fonctions::CheckIfNotLoggedIn($_SESSION['login']);
-    $bdd = Fonctions::InitBDD();
+function table_ok($table, $bdd){
+    $req = $bdd->query("SHOW TABLES LIKE '$table'");
+    $tableExists = $req !== false && $req->rowCount() > 0;
+    if ($tableExists)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+if(!table_ok("reservation", $bdd))
+{
+    Fonctions::RequeteSQLExecute($bdd,
+        "CREATE TABLE reservation (
+            ID int(10) NOT NULL,
+            Nom varchar(32) NOT NULL,
+            date date NOT NULL,
+            véhicule varchar(32) NOT NULL,
+            Nombre_covoit int(11) NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+    
+
+    Fonctions::RequeteSQLExecute($bdd,"ALTER TABLE reservation ADD PRIMARY KEY (ID);");
+
+    Fonctions::RequeteSQLExecute($bdd,"ALTER TABLE reservation MODIFY ID int(10) NOT NULL AUTO_INCREMENT",$bdd);
+    
+    Fonctions::RequeteSQLExecute($bdd,"COMMIT");
+
+
+    
+}
+
+
+
 
 if(isset($_POST['submit']))
 {
